700f7349bd42ab3a68c7b1b2b5e76f1488fea75c
Jordin Brown <jbrown@cognetif.com>
2019-05-10 11:24:13 -0400

    Move to https://github.com/cognetif/cognetif_apimail

--- addons/apps/cognetif_apimail/util/autoloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Add Composer autoloader for vendor dependencies
- */
-require_once(__DIR__ . "/../vendor/autoload.php");
--- addons/apps/cognetif_apimail/util/events.php
+++
@@ -1,3 +0,0 @@
-<?php
-$API = new PerchAPI(1.0, 'cognetif_apimail');
-$API->on('email.send', 'Cognetif\ApiMail\Manager::email_send');
